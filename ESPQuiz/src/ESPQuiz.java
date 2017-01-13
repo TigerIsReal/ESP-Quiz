04f476a28f733dbac0851e4269bc7f5ffabd4efa
Tiger Sheng <tiger981120@gmail.com>
2017-01-12 19:32:45 -0500

    Add files via upload
    Hot Fix
    2017-01-12

--- ESPQuiz/src/ESPQuiz.java
+++ ESPQuiz/src/ESPQuiz.java
@@ -2,8 +2,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 
 
@@ -12,26 +15,31 @@ import java.util.Random;
  */
 public class ESPQuiz extends JFrame implements ActionListener {
 
+    private static List<QuizQuestion> quizQuestionsList;
+    private static QuizQuestions quizQuestions;
     private final Font font1 = new Font("Arial", Font.BOLD, 20);
     private final Font font2 = new Font("Arial", Font.BOLD, 28);
+    private final String[] op = {"A", "B", "C", "D"};
     private JPanel p1, p2, p3;
+    private JPanel pi;
     private JLabel lb1;
     private JLabel[] lbs;
+    private JLabel reason;
     private int questionNum = 0;
     private JButton[] buttons;
-    private static List<QuizQuestion> quizQuestionsList;
     private int randCorrectNum;
     private int score = 0;
-    private int t = 20;
+    private int t = 30;
+    private int totalTime = 0;
     private Timer tim;
     private boolean finished = false;
-    private static QuizQuestions quizQuestions;
 
     public static void main(String args[]) {
         ESPQuiz q = new ESPQuiz();  //make object ESPQuiz
         q.prepGUI();  //prep for GUI
         q.prepQuestions();  //prep for Question panel
         q.makeGUI();  //make the GUI
+        q.makeQuestions();  //make questions
         q.tim.start();  //start timer
     }
 
@@ -54,7 +62,7 @@ public class ESPQuiz extends JFrame implements ActionListener {
         return answers;
     }
 
-    public static void refresh(){
+    public static void refresh() {
         quizQuestions = new QuizQuestions();
         quizQuestionsList = new ArrayList<>();
         quizQuestionsList = quizQuestions.getQuizQuestions();  //get questions
@@ -83,7 +91,13 @@ public class ESPQuiz extends JFrame implements ActionListener {
             int help = JOptionPane.showOptionDialog(null, "Sorry :( Wrong answer. Your score: " + score, "Your Score", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
             // show popup with an option to access help
             if (help == 1) {  // if the user needs help
-                JOptionPane.showMessageDialog(null, quizQuestionsList.get(questionNum).getReason());
+                pi = new JPanel();  //create dialog panel
+                reason = new JLabel();  //create reason label
+                reason.setPreferredSize(new Dimension(250, 400));  //set size
+                reason.setFont(font1);  //set font
+                reason.setText("<html>" + quizQuestionsList.get(questionNum).getReason() + "The correct answer is " + op[randCorrectNum] + ".</html>");  //set reason
+                pi.add(reason);  //add reason
+                JOptionPane.showMessageDialog(null, pi);
                 // show the user the help documentation
             }
         }
@@ -95,66 +109,85 @@ public class ESPQuiz extends JFrame implements ActionListener {
             int result = JOptionPane.showConfirmDialog(null, "Want to start over again?", "", JOptionPane.YES_NO_OPTION);
             //ask user if they want to start over again
             if (result == 0) {  // if they do
-                t = 20;  // reset time count
+                t = 30;  // reset time count
                 tim.start();  //restart timer
                 questionNum = 0;  // restart quiz
                 score = 0;  // reset their score
-                p2.removeAll();  // reset question panel
-                lb1.setText("ESP Quiz                                  Time Left: " + t);  //reload time count panel
+                p2.revalidate();  // reset question panel
+                lb1.setText("Score: " + score + "                   ESP Quiz                    Time Left: " + t);  //reload time count panel
                 refresh();  //load questions
                 prepQuestions();  // reload questions
-                makeGUI();  // remake GUI
+                makeQuestions();
+                p2.repaint();  //repaint question panel
                 finished = false;  // turn finished to false
             } else {  // if the user wants to quit
                 System.exit(0);  //exit
             }
         } else {  // if there are still questions left in the quiz
             questionNum += 1;  //add 1 to question number
-            t = 20;  //reset time count
+            t = 30;  //reset time count
             tim.start();  //restart timer
-            p2.removeAll();  //reset question panel
-            lbs[0].removeAll();
-            lb1.setText("ESP Quiz                                  Time Left: " + t);  //reset time count label
+            p2.revalidate();  //reset panel
+            lb1.setText("Score: " + score + "                   ESP Quiz                    Time Left: " + t);  //reload time count panel
             prepQuestions();  //reload questions
-            makeGUI();  //remake GUI
+            makeQuestions();  //remake question panel
+            p2.repaint();  //repaint question panel
         }
     }
 
     private void prepGUI() {
         this.setTitle("ESP Quiz");  //set frame title
         setSize(1024, 768);  //set frame size
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);  //set close operation
         setLocationRelativeTo(null);  //set location
+
+        setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);  //set close operation
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                JFrame frame = (JFrame)e.getSource();
+                tim.stop();
+                Object[] options = {"Stay!", "Don't Leave!"};
+                int quit = JOptionPane.showOptionDialog(frame,"Are you sure you want to quit? :(","ESP needs you!",JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE,null,options,options[0]);
+                if(quit == 1){
+                    System.exit(0);
+                }
+                else{
+                    tim.start();
+                }
+            }
+        });
 
         GridBagLayout gridB = new GridBagLayout();
         setLayout(gridB);  //set layout for the frame
 
         p1 = new JPanel();
         p1.setLayout(gridB);  //set p1 layout
-        p1.setPreferredSize(new Dimension(600, 75));  //set p1 size
+//        p1.setBorder(BorderFactory.createLineBorder(Color.black));
 
-        lb1 = new JLabel("ESP Quiz                                  Time Left: " + t);  //set timer label
+        score = 0;
+        lb1 = new JLabel();
+        lb1.setText("Score: " + score + "                   ESP Quiz                    Time Left: " + t);  //reload time count panel
         lb1.setFont(font1);
         //define timer
         tim = new Timer(1000, e -> {
-            if (t > 15 && !finished) {  //if 15 or more seconds is left and quiz not finished
+            if (t > 1 && !finished) {  //if 1 or more seconds is left and quiz not finished
                 t--;  //timer count down 1
-                lb1.setText("ESP Quiz                                  Time Left: " + t);  //reset timer label
-                //but do not take away marks
-            } else if (t <= 15 && t > 1 && !finished) {  //if less than 15 seconds is left and quiz not finished
-                t--;  //timer count down 1
-                score--;  //take away 1 mark per second
-                lb1.setText("ESP Quiz                                  Time Left: " + t);  //reset timer label
+                lb1.setText("Score: " + score + "                   ESP Quiz                    Time Left: " + t);  //reload time count panel
             } else if (t == 1 && !finished) {  //if only 1 second left and quiz not finished
                 t--;  //timer count down 1
-                score--;  //take away 1 mark
                 tim.stop();  //stop timer
-                lb1.setText("ESP Quiz                                  Time Left: " + t);  //reset timer label
+                lb1.setText("Score: " + score + "                   ESP Quiz                    Time Left: " + t);  //reload time count panel
                 Object[] options = {"OK", "Help!"};
                 int help = JOptionPane.showOptionDialog(null, "Sorry :( Time's out. Your score: " + score, "Your Score", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
                 //popup show time's out and attempt user to use help
                 if (help == 1) {
-                    JOptionPane.showMessageDialog(null, quizQuestionsList.get(questionNum).getReason());
+                    pi = new JPanel();  //make dialog panel
+                    reason = new JLabel();  //make label
+                    reason.setPreferredSize(new Dimension(250, 400));  //set size
+                    reason.setFont(font1);  //set font
+                    reason.setText("<html>" + quizQuestionsList.get(questionNum).getReason() + "The correct answer is " + op[randCorrectNum] + ".</html>");  //set reason
+                    pi.add(reason);  //add reason
+                    JOptionPane.showMessageDialog(null, pi);
                     //show help popup
                 }
                 if (questionNum == quizQuestionsList.size() - 1) {  //if no question is left
@@ -166,38 +199,49 @@ public class ESPQuiz extends JFrame implements ActionListener {
                     //ask user to start over again
                     if (result == 0) {  //if yes
                         questionNum = 0;  //reset question number
-                        t = 20;  //reset timer count
+                        t = 30;  //reset timer count
                         tim.start();  //restart timer
-                        p2.removeAll();  //reset question panel
-                        lb1.setText("ESP Quiz                                  Time Left: " + t);  //reset timer label
-                        refresh();
+                        p2.revalidate();  //reset question panel
+                        lb1.setText("Score: " + score + "                   ESP Quiz                    Time Left: " + t);  //reload time count panel
+                        refresh();  //reload questions
                         prepQuestions();  //reload questions
-                        makeGUI();  //remake GUI
+                        makeQuestions();  //remake questions
+                        p2.repaint();  //repaint
                         finished = false;  //turn finished to false
                     } else {  //if user wants to quit
                         System.exit(0);
                     }
                 } else {  //if still questions left
                     questionNum += 1;  //add 1 to question number
-                    t = 20;  //reset time count
+                    t = 30;  //reset time count
                     tim.start();  //restart timer
-                    lbs[0].removeAll();
-                    p2.removeAll();  //reset question panel
-                    lb1.setText("ESP Quiz                                  Time Left: " + t);  //reset timer label
+                    p2.revalidate();  //reset question panel
+                    lb1.setText("Score: " + score + "                   ESP Quiz                    Time Left: " + t);  //reload time count panel
                     prepQuestions();  //reload questions
-                    makeGUI();  //remake GUI
+                    makeQuestions();  //remake questions
+                    p2.repaint();  //repaint questions
                 }
+            }
+
+            if(totalTime < 49){
+                totalTime++;
+            }else if(totalTime == 49){
+                totalTime++;
+                score--;
+                totalTime = 0;
             }
         });
 
         p2 = new JPanel();  //make question panel
-        p2.setPreferredSize(new Dimension(300, 425));  //set size
         p2.setLayout(new BoxLayout(p2, BoxLayout.Y_AXIS)); //set layout
         refresh(); //load questions
+//        p2.setBorder(BorderFactory.createLineBorder(Color.black));
+
 
         p3 = new JPanel();  //make buttons panel
-        p3.setPreferredSize(new Dimension(600, 100));  //set size
         p3.setLayout(new FlowLayout(FlowLayout.CENTER, 40, 30));  //set layout
+//        p3.setBorder(BorderFactory.createLineBorder(Color.black));
+
 
         buttons = new JButton[4];  //make buttons
         for (int i = 0; i < 4; i++) {
@@ -251,24 +295,24 @@ public class ESPQuiz extends JFrame implements ActionListener {
         lbs = new JLabel[5];  //make question labels
         for (int i = 0; i < 5; i++) {
             lbs[i] = new JLabel();
+            lbs[i].setPreferredSize(new Dimension(600, 200));
+            lbs[i].setFont(font1);
             switch (i) {  //input question
                 case 0:
-                    lbs[i].setText(quizQuestionsList.get(questionNum).getQuestion());  //question body
+                    lbs[i].setText("<html>" + (questionNum+1) + ". " + quizQuestionsList.get(questionNum).getQuestion() + "</html>");  //question body
                     break;
                 case 1:
-                    lbs[i].setText("A. " + cAnswerList[i - 1]);  //choice A
+                    lbs[i].setText("<html>A. " + cAnswerList[i - 1] + "</html>");  //choice A
                     break;
                 case 2:
-                    lbs[i].setText("B. " + cAnswerList[i - 1]);  //choice B
+                    lbs[i].setText("<html>B. " + cAnswerList[i - 1] + "</html>");  //choice B
                     break;
                 case 3:
-                    lbs[i].setText("C. " + cAnswerList[i - 1]);  //choice C
+                    lbs[i].setText("<html>C. " + cAnswerList[i - 1] + "</html>");  //choice C
                     break;
                 case 4:
-                    lbs[i].setText("D. " + cAnswerList[i - 1]);  //choice D
+                    lbs[i].setText("<html>D. " + cAnswerList[i - 1] + "</html>");  //choice D
             }
-            lbs[i].setPreferredSize(new Dimension(600, 100));
-            lbs[i].setFont(font1);
         }
     }
 
@@ -282,29 +326,36 @@ public class ESPQuiz extends JFrame implements ActionListener {
         g.gridy = 0;
         g.gridwidth = 3;
         g.weightx = 1;
-        g.ipady = 50;
+        g.ipady = 0;
+        g.insets = new Insets(50, 0, 0, 0);
         add(p1, g);  //add timer panel
 
-        for (JLabel l : lbs) {  //add question labels
-            p2.add(l, gbc);
-        }
-        g.fill = GridBagConstraints.HORIZONTAL;
-        g.gridx = 0;
-        g.gridy = 1;
-        g.weighty = 1.0;
-        g.ipady = 300;
-        g.insets = new Insets(0, 50, 0, 50);
-        add(p2, g);  //add question panel
-
-        g.fill = GridBagConstraints.HORIZONTAL;
         g.ipady = 0;
-        g.gridx = 0;
         g.gridy = 2;
-        g.insets = new Insets(0, 0, 0, 0);
+        g.weighty = 0;
+        g.insets = new Insets(0, 0, 25, 0);
         add(p3, g);  //add button panel
         for (JButton b : buttons) {  //add buttons
             p3.add(b, gbc);
         }
         setVisible(true);  //set visible
+    }
+
+    private void makeQuestions() {
+        GridBagConstraints gbc = new GridBagConstraints();
+        GridBagConstraints g = new GridBagConstraints();
+
+        p2.removeAll();
+        for (JLabel l : lbs) {  //add question labels
+            p2.add(l, gbc);
+        }
+        g.fill = GridBagConstraints.HORIZONTAL;
+        g.gridy = 1;
+        g.gridwidth = 3;
+        g.weightx = 1.0;
+        g.weighty = 1.0;
+        g.ipady = 300;
+        g.insets = new Insets(0, 50, 0, 50);
+        add(p2, g);  //add question panel
     }
 }
